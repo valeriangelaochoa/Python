0daa2a13e5f331e30a0a46e9cfbdc7f30eb5ecef
valeria <valeriaangelaochoa@iesmontsia.org>
2025-11-13 19:15:42 +0100

    commit2

--- Training3_majors_d'edat.py
+++ Training3_majors_d'edat.py
@@ -1,5 +1,5 @@
 edat = int(input("escriu la teva edat:"))
 if edat >= 18:
    print("Ets major d'edat")
-else:
-    print("Ets menor d'edat")    
+else: 
+   print("Ets menor d'edat")
---
+++ Training_5_Funcions.py
@@ -0,0 +1,85 @@
+def area_costat():
+    costat = float(input("Introdueix la mida del costat del quadrat:"))
+    area = costat * costat
+    print("L'àrea del quadra")
+
+def nombres_enters():
+        print("Calcularen la suma, la resta, la multiplicació i la divisió")
+        Número1 = float (input("Introdueix el primer número enter:"))
+        Número2 = float (input("Introdueix el segon número enter:"))
+        suma = Número1 + Número2
+        resta = Número1 - Número2
+        multiplicació = Número1 * Número2
+        divisió = Número1 / Número2
+        print ("El resultat de la suma és:", int(suma), "El resultat de la resta és:", int(resta), "El resultat de la multiplicació és", int(multiplicació), "El resultat de la divisió és", int(divisió))
+    
+def crear_frase():
+        print("Introdueix les tres paraules per a crar la frase")
+        paraula1 = input("Introdueix la primera paraula")
+        paraula2 = input("Introdueix la segona paraula")
+        paraula3 = input("Introdueix la tercera paraula")
+        Frase_feta = "Llamame a " + paraula1 + " dile que estoy en el " + paraula2 + " i que tengo que hablar de " + paraula3 
+        print (Frase_feta)
+    
+def suma_nombres():
+        print("Introduiex dos nombres")
+        numero1 = float(input("Introdueix el primer número"))
+        numero2 = float(input("Introdueix el segon número"))
+        suma = numero1 + numero2
+        print("El resultat de la suma és", round(suma))
+
+def majors_edat():
+    edat = int(input("escriu la teva edat:"))
+    if edat >= 18:
+        print("Ets major d'edat")
+    else:
+        print("Ets menor d'edat")
+
+def nombre_gran():
+    Nom1= input("Escriu el primer nombre:")
+    Nom2= input("Escriu el segon nombre:")
+    Nom3= input("Escriu el tercer nombre:")
+    if Nom1 >= Nom2:
+        if Nom1 >= Nom3:
+            print("El nombre més gran és:", Nom1)
+    if Nom2 >= Nom1:
+        if Nom2 >= Nom3:
+            print("El nombre més gran és:", Nom2)
+    if Nom3 >= Nom1:
+        if Nom3 >= Nom2:
+            print("El nombre més gran és", Nom3)
+
+def positiu_o_negatiu():
+    Nombre = input("Escriu el nombre que vulguis: ")
+    if int(Nombre) >= 0:
+        print("Es un nombre positiu")
+    else:
+        print("Es un nombre negatiu")
+
+def nombre_impars():
+    print("Nombres impars entre 1 i 200: ")
+    for v in range(1,201):
+        if v %2 == 0:
+            print(v)
+
+def nombre_10():
+    Nota = 0
+    flap = 0
+    while Nota != -1:
+        Nota = int(input("Introdueix un nombre entre 0-10 per a finalitzar: "))
+        if Nota ==10:
+            flap= 1
+    if flap == 1:
+        print("Hi ha hagut alguna nota que té un 10")
+    else:
+        print("No hi ha cap 10")
+
+def nombre_negatiu():
+    Notes = [-5,-4,-3,-2,-1,0,1,2,3,4]
+    for Nombre in Notes:
+        print(Nombre)
+    if min(Notes) < 10:
+        print("Hi habia almenys un nombre negatiu")
+    else:
+        print("No hi ha cap nombre negatiu")
+
